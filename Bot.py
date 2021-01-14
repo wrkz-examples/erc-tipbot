4b8e8efd72d72af902ce607310dd99927950e412
Jacobvs <jacobvs2@gmail.com>
2021-01-13 23:20:01 -0800

    Add extension loading function
    Signed-off-by: Jacobvs <jacobvs2@gmail.com>

--- Bot.py
+++ Bot.py
@@ -113,6 +113,9 @@ bot.remove_command('help')
 bot.owner_id = config.discord.ownerID
 bot.TX_IN_PROCESS = []
 
+for filename in os.listdir('./cogs/'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
 # Create ETH
 def create_eth_wallet():
